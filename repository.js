462e3dec8b2dbb6f9f6f3a13dcb24b5c7ce27cce
Mike Yoon <mike@yaki.org>
2011-08-20 18:20:07 -0700

    rename db.js

--- repository.js
+++ repository.js
@@ -5,3 +5,14 @@
  * Time: 5:29 PM
  * To change this template use File | Settings | File Templates.
  */
+var Mongolian = require('mongolian');
+var server = new Mongolian();
+var db = server.db('db');
+
+module.exports = {
+    users:
+        db.collection('users'),
+
+    messages:
+        db.collection('messages')
+}
