5dc97f80e52d9d9f8934d6a173081235e2ee2ba8
Mike Yoon <mike@yaki.org>
2011-09-08 00:51:35 -0700

    an attempt at a web test

--- tests/login.js
+++ tests/login.js
@@ -5,3 +5,16 @@
  * Time: 6:58 PM
  * To change this template use File | Settings | File Templates.
  */
+
+var tobi = require('tobi');
+
+module.exports.boot = function(app) {
+
+    var browser = tobi.createBrowser(3000, 'localhost');
+    browser.get('/', function(res, $) {
+        $('#form1').fill({ username: 'test3', password: 'test' });
+        $('input[type=submit]').click(function(res, $) {
+            //$('#chatwindow').should.have.one('br');
+        });
+    });
+};
